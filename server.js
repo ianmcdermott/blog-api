0fd746f9458007765025d76fc24b5c803a3d398c
Ian McDermott <imcd.po@gmail.com>
2017-10-18 13:08:33 -0400

    Update Testing

--- server.js
+++ server.js
@@ -17,3 +17,35 @@ app.use('/blogPostRouter', blogPostRouter);
 app.listen(process.env.PORT || 8080, () =>{
 	console.log(`App is listening on port ${process.env.PORT || 8080}`);
 });
+let server;
+//run the server
+function runServer(){
+	const port = process.env.PORT || 8080;
+	return new Promise((resolve, reject)=>{
+		server = app.listen(port, () => {
+			console.log(`Your app is listening on port ${port}`);
+			resolve(server);
+		}).on('error', err => {
+			reject(err)
+		})
+	});
+}
+
+function closeServer(){
+	return new Promise((resolve, reject) =>{
+		console.log('Closing Server');
+		server.close(err =>{
+			if(err){
+				reject(err);
+				return;
+			}
+			resolve();
+		})
+	})
+}
+
+if(require.main === module){
+	runServer()catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test.js
@@ -0,0 +1,88 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Blog Post', function(){
+	before(function(){
+		return runServer();
+	});
+
+	after(function(){
+		return closeServer();
+	});
+
+	it('Should list items on GET'. function(){
+		return chai.request(app)
+			.get('/blogPostRouter')
+			.then(function(res){
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('array');
+
+				res.body.length.should.be.at.least(1);
+
+				const expectedKeys = ['id', 'title', 'content', 'author', 'publishDate'];
+				res.body.forEach(function(item){
+					item.should.be.a('object');
+					item.should.include.keys(expectedKeys)
+				});
+			});
+		});
+
+	it("Should POST item", function(){
+		return chai.request(app);
+		const newItem = {
+			title: 'Test Title', 
+			content: 'Test content',
+			author: 'Test author',
+			publishDate: '1/1/18'}
+		.post('/blogPostRouter')
+		.send(newItem)
+		.then(function(res){
+			res.should.have.status(201);
+			res.should.be.json;
+			res.body.should.be.a('object');
+			res.body.should.include.keys('id', 'title', 'content', 'author', 'publishDate');
+			res.body.id.should.not.be.null;
+			res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+		});
+	});
+
+	it('Should replace items on PUT', function(){
+		const update = {
+			title: 'Update Title', 
+			content: 'Update content',
+			author: 'Update author',
+			publishDate: '1/1/19'}
+		}
+		return chai.request(app)
+			.get('/blogPostRouter')
+			.then(function(res){
+				updateData.id = res.body[0].id;
+
+				return chai.request(app)
+					.put(`/blogPostRouter/${updateData.id}`)
+					.send(updateData)
+			})
+			.then(function(res){
+				res.should.have.status(204);
+			});
+		});
+
+	it('Should delete item on DELETE', function(){
+		return chai.request(app)
+			.get('/blogPostRouter')
+			.then(function(res){
+				return chai.request(app)
+					.delete(`/blogPostRouter/${res.body[0].id}`);
+			})
+			.then(function(res){
+				res.should.have.status(204);
+			});
+	});
+})
